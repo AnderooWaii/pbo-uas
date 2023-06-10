0791b0cf577e716f4ac1848aeef5a56a7075e2fa
Mangde Satya <mangde@itsavirus.com>
2023-06-11 00:35:12 +0800

    Init Pbo

---
+++ src/com/Book.java
@@ -0,0 +1,22 @@
+
+package com;
+
+public abstract class Book {
+    protected String name;
+    protected String category;
+
+    public Book(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public abstract void displayInfo();
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
---
+++ src/com/NonFictionBook.java
@@ -0,0 +1,26 @@
+
+package com;
+public class NonFictionBook extends Book {
+    private String topic;
+
+    public NonFictionBook(String name, String category, String topic) {
+        super(name, category);
+        this.topic = topic;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Non-Fiction Book:");
+        System.out.println("Name: " + name);
+        System.out.println("Category: " + category);
+        System.out.println();
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+}
